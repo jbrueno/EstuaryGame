5e9de5a1a7d4e1ceb55a6f669412c20d7da3df93
Hannah <hmmartin@udel.edu>
2019-11-05 16:49:53 -0500

    setXY method added

--- src/pkgMover/Mover.java
+++ src/pkgMover/Mover.java
@@ -46,39 +46,17 @@ public abstract class Mover {
 	}
 	
 	/**
-	 * Moves the mover towards the given x & y coordinates on the background.
-	 * 
-	 * (assuming this is called every update, and therefore only operates
-	 * once, and if the location is different than the given coordinate)
-	 * 
+	 * Moves to the x and y locations
 	 * 
 	 * @author HM
 	 * 
 	 * @param new_x the double value of the desired x location
 	 * @param new_y the double value of the desired y location
 	 */
-	public void moveTowards(double new_x, double new_y) {
-		// checks if current location is to the left of the new location, minus what the next increase in movement would be
-		if (x < new_x - xIncr) {
-			// if the xIncr is currently moving to the left (negative), it makes it positive
-			if (xIncr <= 0) {
-				xIncr *= -1;
-			}
-			//add the new increase
-			x += xIncr;
-		// checks if the current location is to the right of the new location, 	
-		} else if(x > new_x + xIncr) {
-			//if the xIncr is currently going to move the mover to the right (positive), it makes
-			// the xIncr negative
-			if (xIncr >= 0) {
-				xIncr *= -1;
-			}
-			//add the new increase
-			x += xIncr;
-		}
-		// if the new_x is neither > x or < x, then the mover is already at the location 
+	public void setXY(double newx, double newy) {
+		this.x = newx;
+		this.y = newy;
 	}
-	
 	
 
 }
