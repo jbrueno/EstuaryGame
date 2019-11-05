7699224f46d3917955d8fc1bdf2f2d1a2689d760
Ryan Peters <rppeters@udel.edu>
2019-11-05 16:13:06 -0500

    minor spacing fixes

--- src/pkgMGView/MainScreenView.java
+++ src/pkgMGView/MainScreenView.java
@@ -34,7 +34,6 @@ public class MainScreenView extends MinigameView {
     	setUpListeners();
 
 		importImages();
-
 	}
 	
 	public void update(ArrayList<Mover> ms, GameState gs) {
@@ -87,16 +86,16 @@ public class MainScreenView extends MinigameView {
 	
 	private void setUpButtons() {
 		btnSC = new Button();
-		makeCircleButton(btnSC, 909.0, 482.0);
+		formatCircleButton(btnSC, 909.0, 482.0);
 		
 		btnHCC = new Button();
-		makeCircleButton(btnHCC, 417.0, 587.0);
+		formatCircleButton(btnHCC, 417.0, 587.0);
 		
 		btnAM = new Button();
-		makeCircleButton(btnAM, 852.0, 202.0);
+		formatCircleButton(btnAM, 852.0, 202.0);
 		
 		btnWS = new Button();
-		makeCircleButton(btnWS, 498.0, 85.0);
+		formatCircleButton(btnWS, 498.0, 85.0);
 		
 		root.getChildren().add(btnSC);
 		root.getChildren().add(btnHCC);
@@ -105,7 +104,13 @@ public class MainScreenView extends MinigameView {
 		
 	}
 	
-	private void makeCircleButton(Button b, double x, double y) {
+	/**
+	 * Formats an existing Button to be of a Circle shape
+	 * @param b
+	 * @param x
+	 * @param y
+	 */
+	private void formatCircleButton(Button b, double x, double y) {
 		b.setLayoutX(x);
 		b.setLayoutY(y);
 		b.setShape(new Circle(btnSize));
@@ -125,6 +130,13 @@ public class MainScreenView extends MinigameView {
 		gc.drawImage(loadImage("backgrounds","background_MainScreen.png"), 0, 0, backgroundWidth, backgroundHeight);
 	}
 	
+	/**
+	 * Loads all images to class attributes to prevent unnecessary re-loading of images for faster access time during drawing.
+	 * Currently, only background for the alpha code
+	 * 
+	 * @author Ryan Peters	
+	 * @see loadImage()
+	 */
 	@Override 
 	void importImages() {
 		background = loadImage("backgrounds","background_MainScreen.png");
