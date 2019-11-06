7601e33147c6f18d4f762dcfe267648232b3ba64
Hannah <hmmartin@udel.edu>
2019-11-06 13:43:07 -0500

    added to SCModel

--- src/pkgMG/SCModel.java
+++ src/pkgMG/SCModel.java
@@ -4,6 +4,7 @@ package pkgMG;
 import java.util.ArrayList;
 
 import javafx.scene.input.MouseEvent;
+import pkgEnum.GameState;
 import pkgMover.Mover;
 import pkgMover.Terrapin;
 
@@ -17,6 +18,8 @@ public class SCModel extends MinigameModel{
 	ArrayList<Mover> obstacles = new ArrayList<Mover>();
 	ArrayList<Mover> food = new ArrayList<Mover>();
 	double waterThreshold;
+	final long startNanoTime = System.nanoTime();
+	GameState gameState;
 	
 	/**Constructor that will be given information on the Terrapins
 	 *  starting location, the movers and food currently onscreen
@@ -26,15 +29,17 @@ public class SCModel extends MinigameModel{
 	 */
 	public SCModel(int cw, int ch, int tw, int th, double wt){
 		this.canvasHeight = ch;
+		terry.y = (canvasHeight/2);
 		this.canvasWidth = cw;
 		this.terrapinHeight = th;
 		this.terrapinWidth = tw;
 		this.waterThreshold = wt;
+		gameState = GameState.START;
 	}
 
 	@Override
 	void update(MouseEvent me) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
@@ -46,6 +51,16 @@ public class SCModel extends MinigameModel{
 			terry.airAmount =- 0.1;
 			System.out.println("Terrapin air level " + terry.airAmount);
 		}
+		
+		for (Mover m : obstacles) {
+			if (terry.x >= m.x - m.imageWidth && terry.x <= m.x + m.imageWidth) {
+				if (m.species.equals("seaweed")) {
+					
+				}
+			}
+		}
+		
+		
 		
 	}
 
--- src/pkgMGView/SCView.java
+++ src/pkgMGView/SCView.java
@@ -1,5 +1,48 @@
 package pkgMGView;
 
-public class SCView /*will extend minigameview*/{
+import java.util.ArrayList;
+
+import pkgEnum.GameState;
+import pkgMover.Mover;
+
+public class SCView extends MinigameView {
+
+	@Override
+	public void update(ArrayList<Mover> ms, GameState gs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void startTimer(int ms) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void stopTimer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void setUpListeners() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void importImages() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 
 }
