53dc567832aab4cc77e991ec48859e4c428eb9e7
Hannah <hmmartin@udel.edu>
2019-11-06 12:38:00 -0500

    Terrapin: some code

--- src/pkgMover/Terrapin.java
+++ src/pkgMover/Terrapin.java
@@ -1,5 +1,24 @@
 package pkgMover;
 
 public class Terrapin {
+	
+	double airAmount;
+	private String species;
+	
+	public Terrapin() {
+		this.species = "Terrapin";
+		this.airAmount = 100;
+	}
+	
+	/**
+	 * breathe resets the air that is held by the Terrapin to full. Is
+	 * called whenever the Terrapin is above a certain height
+	 * 
+	 *  @author HM
+	 */
+	public void breathe() {
+		this.airAmount = 100;
+		System.out.println("breath taken");
+	}
 
 }
