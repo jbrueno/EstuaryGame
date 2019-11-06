117ddb3bc8a875af31450b81e45ee54e3fc19464
Hannah <hmmartin@udel.edu>
2019-11-05 21:42:02 -0500

    animal matching model

--- src/pkgMG/AMModel.java
+++ src/pkgMG/AMModel.java
@@ -1,5 +1,13 @@
 package pkgMG;
 
+import pkgEnum.GameType;
+import pkgMover.Mover;
+
 public class AMModel {
+	
+	private final GameType gameType = GameType.ANIMALMATCHING;
+	Mover[] animals;
+	
+	
 
 }
