020940a17ff86b4715f3176a4351e35bcc33a829
Ryan Peters <rppeters@udel.edu>
2019-11-20 21:41:00 -0500

    edited MatchingAnimal values

--- src/pkgMGModel/AMModel.java
+++ src/pkgMGModel/AMModel.java
@@ -71,12 +71,12 @@ public class AMModel extends MinigameModel {
 	 *@author Ryan Peters, Andrew Brenner
 	 */
 	public void createAnimals() {
-		animals.add(new MatchingAnimal(100, 100, 50, 50, "Snowy Grouper", new String[] {
+		animals.add(new MatchingAnimal(100, 100, 50, 50, "SnowyGrouper", new String[] {
 				"I can grow to up to 70 pounds!",
 				"I am very rare to find in the Delaware Bay!",
 				"I have pearly-white spots!",
 				"I am a Snowy Grouper!"})); 
-		animals.add(new MatchingAnimal(400, 400, 50, 50, "White-Tailed Deer",new String[] {
+		animals.add(new MatchingAnimal(400, 400, 50, 50, "WhiteTailedDeer",new String[] {
 				"While I live on land, I can swim up to 15 mph!",
 				"I use my tail to communicate with others!",
 				"I can grow to almost 4 ft tall!",
@@ -86,7 +86,7 @@ public class AMModel extends MinigameModel {
 				"I can survive above water by trapping water in with my shell!",
 				"I have a shiny black shell!",
 				"I am a Mussel!"})); 
-		animals.add(new MatchingAnimal(200, 700, 50, 50, "Blue Crab", new String[] {
+		animals.add(new MatchingAnimal(200, 700, 50, 50, "BlueCrab", new String[] {
 				"I eat small fish, snails, mussels, and plants!",
 				"I have learned to use my hind legs to help me swim!",
 				"I can grow up to 1 to 2 pounds!",
@@ -96,12 +96,12 @@ public class AMModel extends MinigameModel {
 				"We cluster together on hard surfaces underwater on what we call beds",
 				"I can produce a beautiful pearl!",
 				"I am an oyster!"})); 
-		animals.add(new MatchingAnimal(400, 700, 50, 50, "Horseshoe Crab", new String[]{
+		animals.add(new MatchingAnimal(400, 700, 50, 50, "HSC", new String[]{
 				"I have existed for 220 million years. I'm almost a dinosaur!",
 				"I eat mollusks and crustaceans on the ocean floor!",
 				"I must shed (molt) my shell in order to grow!",
 				"I am a Horseshoe Crab!"}));
-		animals.add(new MatchingAnimal(250, 350, 50, 50, "Black Sea Bass", new String[] {
+		animals.add(new MatchingAnimal(250, 350, 50, 50, "BlackSeaBass", new String[] {
 				"In the fall, I migrate off-shore into coastal waters and the ocean",
 				"I can grow up to 2 feet and 9 pounds!",
 				"I sometimes have a long streamer coming off the top of my tail!",
