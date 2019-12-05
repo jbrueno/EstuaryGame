99ba2550b1260acf9bea5bf2e698798f43804b66
Abrenner <Abrenner@andrews-mbp.home>
2019-12-05 03:09:44 -0500

    Added pop-up Minigame titles on MainScreenView when mouse is over icon

--- src/pkgMGView/MainScreenView.java
+++ src/pkgMGView/MainScreenView.java
@@ -3,15 +3,24 @@ import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.text.*;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.TextAlignment;
 import pkgEnum.Direction;
 import pkgEnum.GameState;
 import pkgEnum.Game;
@@ -33,6 +42,15 @@ public class MainScreenView extends MinigameView {
 	final private double btnAMy = 202.0;
 	final private double btnWSx = 498.0;
 	final private double btnWSy = 85.0;
+	
+	// minigame titles
+	private Label labelSC;
+	private Label labelHSCC;
+	private Label labelAM;
+	private Label labelWS;
+	private final int labelHeight = 50;
+	private final int labelWidth = 400;
+	// locations ...
 	
 	private boolean amPlayed = false;
 	private boolean hsccPlayed = false;
@@ -71,23 +89,50 @@ public class MainScreenView extends MinigameView {
     	});
 		
 		btnSC.setOnAction(e -> {
-				game = Game.SIDESCROLLER;
-				scPlayed = true;
+			game = Game.SIDESCROLLER;
+			scPlayed = true;
+		});
+		btnSC.setOnMouseEntered(e -> {
+			root.getChildren().add(labelSC);
+		});
+		btnSC.setOnMouseExited(e -> {
+			root.getChildren().remove(labelSC);
 		});
 		
+		
 		btnHSCC.setOnAction(e -> {
-				game = Game.HSCCOUNT;
-				hsccPlayed = true;
+			game = Game.HSCCOUNT;
+			hsccPlayed = true;
 		});
+		btnHSCC.setOnMouseEntered(e -> {
+			root.getChildren().add(labelHSCC);
+		});
+		btnHSCC.setOnMouseExited(e -> {
+			root.getChildren().remove(labelHSCC);
+		});
+
 		
 		btnAM.setOnAction(e -> {
 			game = Game.ANIMALMATCHING;
 			amPlayed = true;
 		});
+		btnAM.setOnMouseEntered(e -> {
+			root.getChildren().add(labelAM);
+		});
+		btnAM.setOnMouseExited(e -> {
+			root.getChildren().remove(labelAM);
+		});
+		
 		
 		btnWS.setOnAction(e -> {
 			game = Game.WATERSAMPLING;
 			wsPlayed = true;
+		});
+		btnWS.setOnMouseEntered(e -> {
+			root.getChildren().add(labelWS);
+		});
+		btnWS.setOnMouseExited(e -> {
+			root.getChildren().remove(labelWS);
 		});
 		
 	}
@@ -108,10 +153,11 @@ public class MainScreenView extends MinigameView {
 		btnAM.setDisable(amPlayed);
 		formatCircleButton(btnAM, btnAMx, btnAMy);
 		
+		
 		btnWS = new Button("",new ImageView(loadButtonImage("btnWS")));
 		setButtonBackgroundWhite(btnWS);
 		btnWS.setDisable(wsPlayed);
-		formatCircleButton(btnWS, btnSCx, btnSCy);
+		formatCircleButton(btnWS, btnWSx, btnWSy);
 		
 		root.getChildren().addAll(btnSC, btnHSCC, btnAM, btnWS);
 	}
@@ -129,6 +175,45 @@ public class MainScreenView extends MinigameView {
 		b.setMinSize(btnSize * 2, btnSize * 2);
 		b.setMaxSize(btnSize* 2, btnSize * 2);
 	}
+	
+	
+	private void setUpLabels() {
+		
+		labelSC = new Label("Turtle Run");
+		labelSC.setPrefSize(labelWidth, labelHeight);
+		labelSC.setLayoutX(btnSCx + btnSize - (labelWidth / 2));
+		labelSC.setLayoutY(btnSCy - labelHeight);
+		labelSC.setFont(new Font("Arial", 26));
+		labelSC.setAlignment(Pos.CENTER);
+		labelSC.setTextFill(Color.WHITE);
+		
+		labelHSCC = new Label("Horshoe Crab Tagging");
+		labelHSCC.setPrefSize(labelWidth, labelHeight);
+		labelHSCC.setLayoutX(btnHSCCx + btnSize - (labelWidth / 2));
+		labelHSCC.setLayoutY(btnHSCCy - labelHeight);
+		labelHSCC.setFont(new Font("Arial", 26));
+		labelHSCC.setAlignment(Pos.CENTER);
+		labelHSCC.setTextFill(Color.WHITE);
+		
+		labelAM = new Label("Animal Matching");
+		labelAM.setPrefSize(labelWidth, labelHeight);
+		labelAM.setLayoutX(btnAMx + btnSize - (labelWidth / 2));
+		labelAM.setLayoutY(btnAMy - labelHeight);
+		labelAM.setFont(new Font("Arial", 26));
+		labelAM.setAlignment(Pos.CENTER);
+		labelAM.setTextFill(Color.WHITE);
+		
+		labelWS = new Label("Water Sampling");
+		labelWS.setPrefSize(labelWidth, labelHeight);
+		labelWS.setLayoutX(btnWSx + btnSize - (labelWidth / 2));
+		labelWS.setLayoutY(btnWSy - labelHeight);
+		labelWS.setFont(new Font("Arial", 26));
+		labelWS.setAlignment(Pos.CENTER);
+		labelWS.setTextFill(Color.WHITE);
+	}
+	
+	
+	
 
 	
 	/**
@@ -161,6 +246,7 @@ public class MainScreenView extends MinigameView {
 	public void update(ArrayList<Mover> movers, GameState gs, int score, int time) {
 		if (!areButtonsMade) {
 			setUpButtons();
+			setUpLabels();
 			setUpListeners();
 			createScoreLabel(score);
 			areButtonsMade = true;
