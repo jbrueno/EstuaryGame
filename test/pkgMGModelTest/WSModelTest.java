58e55b7e61e1c822bcb99b29444ae13254ebefdf
Abrenner <Abrenner@andrews-mbp.home>
2019-12-08 19:20:35 -0500

    WSModel testing

--- test/pkgMGModelTest/WSModelTest.java
+++ test/pkgMGModelTest/WSModelTest.java
@@ -1,5 +1,6 @@
 package pkgMGModelTest;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
@@ -14,7 +15,9 @@ import javafx.scene.input.MouseEvent;
 import pkgEnum.Game;
 import pkgEnum.GameState;
 import pkgMGModel.WSModel;
+import pkgMGModel.AMModel.MatchingAnimal;
 import pkgMGModel.WSModel.pHStrip;
+import pkgMGModel.AMModel;
 import pkgMGModel.MinigameModel;
 import pkgMover.Mover;
 
@@ -35,11 +38,17 @@ public class WSModelTest {
 	private static String FILL_BOTTLE = "fillBottle";
 	
 	private Class<?>[] parameterTypes;
+	Class<? extends WSModel> c;
 	
 	
 	@Before
 	public void setUp() throws Exception {
 		WSModel = new WSModel();
+		
+		c = WSModel.getClass();
+		
+		
+
 		
 		parameterTypes = new Class[2];
 		parameterTypes[0] = pkgMover.Mover.class;
@@ -95,6 +104,10 @@ public class WSModelTest {
 	public void update_test() {
 		 
 		WSModel.update(null);
+		Field f = c.getDeclaredField("gs");
+		f.setAccessible(true);
+		f.set(WSModel, GameState.START);
+		
 		 WSModel.getMovers().get(0).setY(750);
 		 MouseEvent me = new MouseEvent(MouseEvent.MOUSE_PRESSED, 500, 500, 0, 0, null, 0, false, false, false, false,
 					true, false, false, false, false, false, null);
